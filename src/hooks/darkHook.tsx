e65cb9720a30a494ef337606fc0201e02c5a989e
KRoses96 <45322919+KRoses96@users.noreply.github.com>
2024-11-11 22:41:39 +0000

    feat: dark theme hook

---
+++ src/hooks/darkHook.tsx
@@ -0,0 +1,25 @@
+import { useEffect, useState } from "react";
+
+const useDarkMode = () => {
+  const [isDarkMode, setIsDarkMode] = useState(() => {
+    return localStorage.getItem("theme") === "dark";
+  });
+
+  useEffect(() => {
+    if (isDarkMode) {
+      document.body.classList.add("dark");
+      localStorage.setItem("theme", "dark");
+    } else {
+      document.body.classList.remove("dark");
+      localStorage.setItem("theme", "light");
+    }
+  }, [isDarkMode]);
+
+  const toggleDarkMode = () => {
+    setIsDarkMode(!isDarkMode);
+  };
+
+  return { isDarkMode, toggleDarkMode };
+};
+
+export default useDarkMode;
