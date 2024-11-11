0b3e519b23368bd061232558bafb4f105b66b572
KRoses96 <45322919+KRoses96@users.noreply.github.com>
2024-11-11 22:42:30 +0000

    fix: weird import error

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,12 +1,13 @@
+import MangaLinkForm from "@/components/MangaLinkForm";
 import React from "react";
-import MangaLinkForm from "../components/mangaLinkForm";
+
+
 
 export default function HomePage() {
-
-        return(
+    return (
         <>
-        <div className="flex h-screen flex-col items-center py-10">
-            <MangaLinkForm />
+            <div className="flex h-screen flex-col items-center py-10">
+                <MangaLinkForm />
             </div>
         </>
     );
