c3d30e2ebf1d88aaa8065cbd9df0d4f2c838074b
KRoses96 <45322919+KRoses96@users.noreply.github.com>
2024-11-11 22:42:47 +0000

    feat: dark mode button

--- src/components/NavigationMenu.tsx
+++ src/components/NavigationMenu.tsx
@@ -1,10 +1,24 @@
-import { Link } from "@tanstack/react-router";
 import React from "react";
+import { Link } from "@tanstack/react-router";
+import { Button } from "./ui/button";
+import useDarkMode from "@/hooks/darkHook";
+import { MoonIcon, SunIcon } from "./icons/icons";
 
 export default function NavigationMenu() {
+
+    const { isDarkMode, toggleDarkMode } = useDarkMode();
+    
     return (
+        
         <nav>
             <ul className="flex gap-2 p-2 text-sm">
+                    
+                    <Button onClick={toggleDarkMode} className="h-10 w-10 border-b-gray-400">
+                        <div>
+                            {isDarkMode ? <MoonIcon /> : <SunIcon />}
+                        </div>
+                    </Button>
+
                 <li>
                     <Link to="/">Home</Link>
                 </li>
