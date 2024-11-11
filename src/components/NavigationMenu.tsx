c51c892bc17402d6a39c75e16ba6c905809bb6e7
KRoses96 <45322919+KRoses96@users.noreply.github.com>
2024-11-11 23:41:16 +0000

    feat: highlight menu

--- src/components/NavigationMenu.tsx
+++ src/components/NavigationMenu.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "@tanstack/react-router";
+import { Link, useLocation } from "@tanstack/react-router";
 import { Button } from "./ui/button";
 import useDarkMode from "@/hooks/darkHook";
 import { MoonIcon, SunIcon } from "./icons/icons";
@@ -8,32 +8,35 @@ export default function NavigationMenu() {
 
     const { isDarkMode, toggleDarkMode } = useDarkMode();
 
+    
+
     return (
 
         <nav>
             <ul className="flex gap-2 p-2 text-sm">
 
-                <Button onClick={toggleDarkMode} className="h-10 w-10 border-b-gray-400">
-                    <div>
+                <Button onClick={toggleDarkMode} className="h-6 w-5 b-5 border-b-gray-400">
+                    <div className="">
                         {isDarkMode ? <MoonIcon /> : <SunIcon />}
                     </div>
                 </Button>
-
-                <li>
+                
+                <li className={useLocation().pathname == '/'? "text-blue-400" : ""}>
                     <Link to="/">Home</Link>
                 </li>
 
-                <li>
-                    <Link to="/library">Library</Link>
+                <li className={useLocation().pathname == '/library'? "text-blue-400" : ""}>
+                    <Link to="/library" >Library</Link>
                 </li>
 
-                <li>
+                <li className={useLocation().pathname == '/add'? "text-blue-400" : ""}>
                     <Link to="/add">Add</Link>
                 </li>
 
-                <li>
+                <li className={useLocation().pathname == '/settings'? "text-blue-400" : ""}>
                     <Link to="/settings">Settings</Link>
                 </li>
+
 
             </ul>
         </nav>
