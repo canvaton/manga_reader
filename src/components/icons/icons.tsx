4d3cf45ca54f8810b1f2bcdd13b33be2fb2d8ff2
KRoses96 <45322919+KRoses96@users.noreply.github.com>
2024-11-11 22:42:03 +0000

    feat: sun + moon icons

---
+++ src/components/icons/icons.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+
+export const MoonIcon = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    stroke="currentColor"
+    className="w-6 h-6"
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth={2}
+      d="M21 12.79A9 9 0 1111.21 3a7 7 0 009.79 9.79z"
+    />
+  </svg>
+);
+
+
+export const SunIcon = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    stroke="currentColor"
+    className="w-6 h-6"
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth={2}
+      d="M12 2v2m6.364 1.636l-1.414 1.414M18 12h2m-1.636 6.364l-1.414-1.414M12 18v2m-4.95-1.636l-1.414 1.414M6 12H4m1.636-6.364L6.05 4.222M12 6a6 6 0 100 12 6 6 0 000-12z"
+    />
+  </svg>
+);
